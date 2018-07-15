7a4799dc54ab58f4ae394ed6eea77cd3edb5ab4a
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2018-07-14 23:06:24 -0300

    Creating RedditNewsItem data class

---
+++ app/src/main/java/com/meteoro/kedditlearn/commons/Models.kt
@@ -0,0 +1,10 @@
+package com.meteoro.kedditlearn.commons
+
+data class RedditNewsItem(
+        val author: String,
+        val title: String,
+        val numComments: Int,
+        val created: Long,
+        val thumbnail: String,
+        val url: String
+)
